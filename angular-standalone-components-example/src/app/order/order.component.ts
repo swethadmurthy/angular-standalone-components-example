05665b7dae5866c344a5a5d0bca57c463782651b
Swetha Dakshinamurthy <swethadmurthy@gmail.com>
2025-03-19 21:56:53 +0000

    initial commit

---
+++ angular-standalone-components-example/src/app/order/order.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  constructor(
+    
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
